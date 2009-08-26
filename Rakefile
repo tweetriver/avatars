82c0498d1d93ec92dd97eb0de3ddba93889108aa
Bruce Williams <bruce@codefluency.com>
2009-08-25 21:03:26 -0700

    Logging

--- Rakefile
+++ Rakefile
@@ -1,3 +1,9 @@
+desc "Deploy to Google App Engine"
 task :deploy do
   sh "appcfg.py update ."
+end
+
+desc "Run"
+task :run do
+  sh "dev_appserver.py ."
 end
--- avatars.py
+++ avatars.py
@@ -1,3 +1,5 @@
+import logging
+
 from google.appengine.api import memcache
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
@@ -14,22 +16,30 @@ class Avatar:
     
   @property
   def url(self):
-    url = memcache.get(self.key)
-    if url is not None:
-      return url
-    else:
-      url = self._verified_guess() or self._url()
-      if url:
-        memcache.add(self.key, url, 60 * 15)
-      return url
+    try:
+      url = memcache.get(self.key)
+      if url is not None:
+        logging.debug("[%s] Retrieved URL from memcache" % self.screen_name)
+        return url
+      else:
+        url = self._verified_guess() or self._url()
+        if url:
+          logging.debug("[%s] Adding URL to memcache" % self.screen_name)
+          memcache.add(self.key, url, 60 * 15)
+        return url
+    except:
+      logging.error('There was an error retrieving information from memcache')
+      
   
   def _verified_guess(self):
       try:
         request = urllib2.Request(self.guessed_url)
         request.get_method = lambda: "HEAD"
         urllib2.urlopen(request)
+        logging.debug("[%s] Verified URL" % self.screen_name)
         return self.guess_url
       except:
+        logging.debug("[%s] Could not verify URL" % self.screen_name)
         return None
   
   def _url(self):
@@ -38,6 +48,7 @@ class Avatar:
       if profile:
         soup = BeautifulSoup(self._profile())
         image = soup.find("img", {"alt": self.screen_name})
+        logging.debug("[%s] Found profile image" % self.screen_name)
         return image['src']
       else:
         return None
@@ -57,13 +68,16 @@ class App(webapp.RequestHandler):
       if avatar.url and len(avatar.url):
         self.response.out.write(avatar.url)
       else:
+        logging.debug("[%s] No avatar URL found" % screen_name)
         self.response.set_status(404, "Not Found")
     else:
+      logging.debug("Screen name not provided")
       self.response.set_status(500, "Error")
 
 application = webapp.WSGIApplication([(r'/(.*)/(.*)', App),
                                       (r'/(.*)', App)])
 def main():
+  logging.getLogger().setLevel(logging.DEBUG)
   run_wsgi_app(application)
 
 if __name__ == "__main__":
